9f65a44e8aead6b3a7a728d4908fa0b0631ca7f6
Fernando Montoya <montogeek@gmail.com>
2016-02-08 23:09:08 -0500

    Added Web styles and updated RN

--- app/components/counter.js
+++ app/components/counter.js
@@ -4,10 +4,33 @@ export default function(props) {
   const { counter, increment, decrement } = props;
 
   return (
-    <div className="screen">
-      <p>{counter}</p>
-      <button onClick={increment}>up</button>
-      <button onClick={decrement}>down</button>
+    <div style={styles.containerStyle}>
+      <p style={styles.value}>{counter}</p>
+      <button style={styles.buttonStyle} onClick={increment}>up</button>
+      <button style={styles.buttonStyle} onClick={decrement}>down</button>
     </div>
   );
+}
+
+const styles = {
+  'containerStyle': {
+    margin: '0 auto',
+    width: '200px',
+    textAlign: 'center',
+    marginTop: '200px'
+  },
+
+  'value': {
+    fontSize: '20px',
+  },
+
+  'buttonStyle': {
+    padding: '15px 20px',
+    border: 0,
+    borderRadius: '5px',
+    backgroundColor: '#cc7a6f',
+    color: '#fafafa',
+    marginRight: '5px',
+    cursor: 'pointer'
+  }
 }
